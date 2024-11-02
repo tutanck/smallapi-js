919652bc58e0d1464a921e3de983d95216d4483e
tutanck <tutanck@gmail.com>
2024-11-02 15:00:11 +0100

    _

--- src/client/connect.js
+++ src/client/connect.js
@@ -9,49 +9,55 @@ import { get } from '../axios/client.js';
 export default function connect(serverUrl, { decodeKey = null }) {
   console.log('-> fetching', serverUrl);
 
-  get(serverUrl).then(async (indexMap) => {
-    let descriptor;
-    let fromConf = false;
+  get(serverUrl)
+    .then(async (indexMap) => {
+      let descriptor;
+      let fromConf = false;
 
-    if (hasConf(indexMap)) {
-      try {
-        const confUrl = `${slashes(serverUrl)}${CONF_ROUTE_URI}`;
+      if (hasConf(indexMap)) {
+        try {
+          const confUrl = `${slashes(serverUrl)}${CONF_ROUTE_URI}`;
 
-        console.log('-> fetching', confUrl);
+          console.log('-> fetching', confUrl);
 
-        const conf = await get(confUrl);
+          const conf = await get(confUrl);
 
-        const { routing } = conf;
+          const { routing } = conf;
 
-        const apiMap = routing[API_BASE_URI];
+          const apiMap = routing[API_BASE_URI];
 
-        if (!(isArray(apiMap) && apiMap.length > 0))
-          throw new Error('Invalid or empty api-map');
+          if (!(isArray(apiMap) && apiMap.length > 0))
+            throw new Error('Invalid or empty api-map');
 
-        descriptor = _.groupBy(apiMap, 'name');
+          descriptor = _.groupBy(apiMap, 'name');
 
-        fromConf = true;
+          fromConf = true;
 
-        console.log(`generating from api map descriptor..`);
+          console.log(`generating from api map descriptor..`);
 
-        console.log(descriptor);
-      } catch (error) {
-        console.error(error);
-        console.log(`api conf unreachable on server "${serverUrl}"`);
-        throw new Error('api conf unreachable on server "${serverUrl}"');
+          console.log(descriptor);
+        } catch (error) {
+          console.error(error);
+          console.log(`api conf unreachable on server "${serverUrl}"`);
+          throw new Error(`api conf unreachable on server "${serverUrl}"`);
+        }
+      } else {
+        console.log(`api conf not found on server "${serverUrl}"`);
+
+        throw new Error(`api conf not found on server "${serverUrl}"`);
       }
-    } else {
-      console.log(`api conf not found on server "${serverUrl}"`);
 
-      throw new Error('api conf not found on server "${serverUrl}"');
-    }
+      const { protocol, host } = parseUrl(serverUrl);
 
-    const { protocol, host } = parseUrl(serverUrl);
+      const serverRootUrl = `${protocol}//${host}`;
 
-    const serverRootUrl = `${protocol}//${host}`;
+      const options = { fromConf, apiBaseUri: API_BASE_URI, decodeKey };
 
-    const options = { fromConf, apiBaseUri: API_BASE_URI, decodeKey };
+      console.log({ descriptor, serverRootUrl, options });
+    })
+    .catch((error) => {
+      console.error(error);
 
-    console.log({ descriptor, serverRootUrl, options });
-  });
+      throw new Error(`Unable to connect to server "${serverUrl}"`);
+    });
 }
