f0956b5669eb8d105ad30600c64a7db0b0b7be88
tutanck <tutanck@gmail.com>
2024-11-02 15:13:58 +0100

    _

--- src/client/connect.js
+++ src/client/connect.js
@@ -21,33 +21,21 @@ export default function connect(serverUrl, { decodeKey = null }) {
 
           const conf = await get(confUrl);
 
-          console.log({ conf });
-
           const { routing } = conf;
 
-          console.log({ routing });
-
           const apiMap = routing[API_BASE_URI];
-
-          console.log({ apiMap });
 
           if (!(isArray(apiMap) && apiMap.length > 0)) {
             throw new Error('Invalid or empty api-map');
           }
 
           descriptor = _.groupBy(apiMap, 'name');
-
-          console.log({ descriptor });
         } catch (error) {
           console.error(error);
-          console.log(`Api conf unreachable on server "${serverUrl}"`);
-          throw new Error(`Api conf unreachable on server "${serverUrl}"`);
-        }
-      } else {
-        console.log(`Api conf not found on server "${serverUrl}"`);
 
-        throw new Error(`Api conf not found on server "${serverUrl}"`);
-      }
+          throw new Error(`Api conf unreachable on server '${serverUrl}'`);
+        }
+      } else throw new Error(`Api conf not found on server '${serverUrl}'`);
 
       const { protocol, host } = parseUrl(serverUrl);
 
@@ -55,11 +43,13 @@ export default function connect(serverUrl, { decodeKey = null }) {
 
       const options = { apiBaseUri: API_BASE_URI, decodeKey };
 
-      console.log({ descriptor, serverRootUrl, options });
+      const config = { descriptor, serverRootUrl, options };
+
+      return config;
     })
     .catch((error) => {
       console.error(error);
 
-      throw new Error(`Unable to connect to server "${serverUrl}"`);
+      throw new Error(`Unable to connect to server '${serverUrl}'`);
     });
 }
